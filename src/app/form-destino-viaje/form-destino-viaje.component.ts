72d8e1f1072fdd7ce2d4d8f1a165bf0699ab43a4
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-10-29 10:17:47 -0600

    validaciones avanzadas: parametrizadas

--- src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { DestinoViaje } from '../models/destino-viaje.models';
 
 @Component({
@@ -10,10 +10,15 @@ import { DestinoViaje } from '../models/destino-viaje.models';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje> = new EventEmitter();
   fg: FormGroup;
+  minLongitud = 3;
 
   constructor(fb: FormBuilder) {
     this.fg = fb.group({
-      nombre: ['', Validators.required],
+      nombre: ['', Validators.compose([
+        Validators.required,
+        this.nombreValidator,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url: ['']
     });
     this.fg.valueChanges.subscribe( (form) => {
@@ -27,6 +32,24 @@ export class FormDestinoViajeComponent implements OnInit {
   guardar(nombre:string, url:string): boolean {
     this.onItemAdded.emit(new DestinoViaje(nombre,url));
     return false;
+  }
+
+  nombreValidator(control: FormControl): { [s:string]: boolean } {
+    let l = control.value.toString().trim().length;
+    if (l>0 && l<5){
+      return { invalidNombre: true};
+    }
+    return null;
+  }
+
+  nombreValidatorParametrizable(minLong: number): ValidatorFn {
+    return (control: FormControl):  { [s:string]: boolean } | null => {
+      let l = control.value.toString().trim().length;
+      if (l>0 && l< minLong){
+        return { minLongNombre: true};
+      }
+      return null;
+    }
   }
 
 }
