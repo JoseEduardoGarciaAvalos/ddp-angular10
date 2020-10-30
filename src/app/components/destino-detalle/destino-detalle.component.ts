7b0356f178b99e58ae777aea6f5dbe0d95aa90b6
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-10-30 14:38:30 -0600

    ID InjectionToken y Decorada (useClass)

--- src/app/components/destino-detalle/destino-detalle.component.ts
+++ src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Injectable, InjectionToken, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.module';
 import { DestinoApiClient } from 'src/app/models/destino-api-client-model';
 import { DestinoViaje } from 'src/app/models/destino-viaje.models';
 
@@ -8,12 +10,34 @@ export class DestinoApiClientViejo {
   }
 }
 
+interface AppConfig {
+  apiEndpoint: string;
+}
+const APP_CONFIG_VALUE: AppConfig = {
+  apiEndpoint: "mi_api.com"
+}
+
+const APP_CONFIG = new InjectionToken<AppConfig>("app.config");
+
+@Injectable()
+export class DestinoApiClientDecorated extends DestinoApiClient {
+  constructor(@Inject(APP_CONFIG) private config:AppConfig, store: Store<AppState>){
+    super(store);
+  }
+
+  getById(id:String):string {
+    console.log("llamando por la API decorada. config: ", this.config.apiEndpoint);
+    return super.getById(id) + " y llamando por la API decorada";
+  }
+}
+
 @Component({
   selector: 'app-destino-detalle',
   templateUrl: './destino-detalle.component.html',
   styleUrls: ['./destino-detalle.component.css'],
   providers: [
-    DestinoApiClient, DestinoApiClientViejo,
+    {provide: APP_CONFIG, useValue: APP_CONFIG_VALUE},
+    {provide: DestinoApiClient, useClass: DestinoApiClientDecorated},
     {provide: DestinoApiClientViejo, useExisting: DestinoApiClient}
   ]
 })
