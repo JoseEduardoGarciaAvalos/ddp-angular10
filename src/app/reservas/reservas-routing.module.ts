1d572dc5c4bd752e44d937d41b6be4103df94cf9
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-10-30 12:08:16 -0600

    modulo feature Reserva con routing

--- src/app/reservas/reservas-routing.module.ts
+++ src/app/reservas/reservas-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReservasDetailComponent } from './reservas-detail/reservas-detail.component';
+import { ReservasListComponent } from './reservas-list/reservas-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"reservas", component: ReservasListComponent},
+  {path:"reservas/:id", component: ReservasDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
