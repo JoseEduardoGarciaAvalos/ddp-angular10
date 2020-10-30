f8d3b95b024390a8787be7aaa71a33b7d13ad879
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-10-30 16:43:07 -0600

    Express API sin persistencia ni apto para deploy

---
+++ express-server/app.js
@@ -0,0 +1,16 @@
+const { json } = require("express");
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.listen(3000, ()  => console.log("Servidor corriendo en puerto 3000"));
+
+app.get("/url", (req,res, next) => res.json(["Paris", "Barcelona", "Barranquilla",
+ "Montevideo", "Santiago de Chile", "México DF", "Nueva York"]));
+
+ var misDestinos = [];
+ app.get("/my", (req,res, next) => res.json(misDestinos));
+ app.post("/my", (req,res, next) => {
+    console.log(req.body);
+    misDestinos = req.body;
+    res.json(misDestinos);
+ });
